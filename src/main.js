4e862a67e16e7fde95380b738d1fc27d1ccb5ed0
kshitij1288 <74482019+kshitij1288@users.noreply.github.com>
2026-09-08 19:39:43 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PrahariDashboard from './PrahariDashboard.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrahariDashboard />
+  </React.StrictMode>
+);
